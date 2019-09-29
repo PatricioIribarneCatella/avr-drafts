5f7a8457ad007a4975e096839410ec71d70b963c
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-09-29 14:29:11 -0300

    Change some files in average

--- average/defs.h
+++ average/defs.h
@@ -1,10 +1,10 @@
-#ifndef AVERAGE_H
-#define AVERAGE_H
+#ifndef DEFS_H
+#define DEFS_H
 
 #define ACUM1 r16
 #define ACUM2 r17
 #define SIZE 8
 #define I r18
 
-#endif // AVERAGE_H
+#endif // DEFS_H
 
