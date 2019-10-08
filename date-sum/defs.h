0fb5af2abdd421a67403c0441389a33f0990e327
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-10-08 12:49:38 -0300

    Add date sum days

---
+++ date-sum/defs.h
@@ -0,0 +1,13 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define F_DIAS r16
+#define F_MES r17
+#define F_ANIO r18
+#define DIAS r19
+#define NF_DIAS r20
+#define NF_MES r21
+#define NF_ANIO r22
+
+#endif // DEFS_H
+
